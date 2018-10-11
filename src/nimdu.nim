dad43c4f2b6acd96ee6d0e7459ed2d93d2fa0040
sh4869221 <sh4869221b@gmail.com>
2018-10-11 12:18:52 +0900

    asS

--- src/nimdu.nim
+++ src/nimdu.nim
@@ -11,14 +11,18 @@ import docopt
 
 let args = docopt(doc, version = "nimdu 0.2.0")
 
-var baseDir: string
+var 
+  baseDir: string
+  fileCount: int
 if args["<dir>"]:
   baseDir = $args["<dir>"]
   echo baseDir
 var size: BiggestInt
 for f in walkDirRec(baseDir):
+  fileCount = fileCount + 1
   try:
     size += f.getFileSize
   except:
     echo fmt"{f}"
 echo fmt"{size.formatSize}"
+echo fmt"file is {filecount}"
